37d3319183e585b981fd6fce3f85b573c7da0f2e
mahdidelavarz <mdelavar903@gmail.com>
2025-09-21 11:02:28 +0330

    esling and next.config updated

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  
-};
-// const withBundleAnalyzer = require('@next/bundle-analyzer')({
-//   enabled: process.env.ANALYZE === 'true',  // Only enable when you set the env var
-// });
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 
-// module.exports = withBundleAnalyzer({
-//   // Your existing Next.js config here (e.g., env, webpack, etc.)
-// });
+};
+
 export default nextConfig;
