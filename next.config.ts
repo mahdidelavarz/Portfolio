39a5983da9f0420bb9af194aa44a2c4da9e3022f
mahdidelavarz <mdelavar903@gmail.com>
2025-10-04 16:12:44 +0330

    deploy error fixed

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // Disables ESLint during builds
   },
-  typescript: {
-    ignoreBuildErrors: true, // Disables TypeScript errors during builds
-  },
-  /* other config options here */
 };
 
 export default nextConfig;
